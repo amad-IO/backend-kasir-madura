7d37c77a66212e8ddc6c2efcbc169cb25789d9c6
istimata <mochistimata@gmail.com>
2025-10-18 00:38:31 +0700

    Initial commit - Backend Kasir Madura

---
+++ src/main/java/com/kasirmadura/backend/BackendKasirmaduraApplication.java
@@ -0,0 +1,13 @@
+package com.kasirmadura.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendKasirmaduraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendKasirmaduraApplication.class, args);
+	}
+
+}
